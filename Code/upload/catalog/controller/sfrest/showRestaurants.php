9d26f2f004d636c6b2950753621ed03c6657bcc2
xxx111x1 <122829105@qq.com>
2015-08-09 02:21:42 -0700

    Change name

---
+++ Code/upload/catalog/controller/sfrest/showRestaurants.php
@@ -0,0 +1,17 @@
+<?php
+header("Content-Type: text/html; charset=gb2312");
+
+class ControllerRestaurantShowRestaurants extends Controller{
+	public function index() {
+		/*if (!$this->customer->isLogged()) {
+			$this->session->data['redirect'] = $this->url->link('account/account', '', 'SSL');
+		
+			$this->response->redirect($this->url->link('account/login', '', 'SSL'));
+		}*/
+		//$filters = $this->request->post['product_id'];
+		$this->load->model('restaurant/information');
+		$data['restaurants'] = $this->model_restaurant_information->getRestaurants();
+		$data['types'] = $this->model_restaurant_information->getTypes();
+		$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/restaurant/showRestaurant.tpl', $data));
+	}
+}
