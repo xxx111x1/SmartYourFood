6c1634299c61698ee15da511ec11e87353c69d59
Terrile <minzou@live.com>
2015-08-09 17:11:59 +0800

    add sffood

---
+++ Code/upload/catalog/model/sffood/food.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Min
+ * Date: 2015/8/9
+ * Time: 15:38
+ */
+class ModelSffoodFood extends Model{
+
+    public function add(){
+
+    }
+
+    public function delete(){
+
+    }
+
+    public function  update(){
+
+    }
+
+    public function  search(){
+
+    }
+
+    public function getFoodByRestID()
+    {
+
+    }
+}
