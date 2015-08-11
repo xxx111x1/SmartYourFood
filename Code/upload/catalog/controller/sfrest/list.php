fb8cee7aa37c51044e753698388704bcefdbfb39
Terrile <minzou@live.com>
2015-08-11 22:39:39 +0800

    remove header.

--- Code/upload/catalog/controller/sfrest/list.php
+++ Code/upload/catalog/controller/sfrest/list.php
@@ -3,11 +3,30 @@
  * Created by PhpStorm.
  * User: Min
  * Date: 2015/8/2
- * Time: 16:04
+ * Time: 16:01
  */
-class ModelShopList extends Model{
-    public function getInfo()
-    {
-        return array("hello","world");
+//http://localhost/?route=shop/list/
+class ControllerSfrestList extends Controller{
+    public function index(){
+        $this->load->model('sfrest/list');
+        //$this->load->model('common/header');
+        //$data = $this->model_shop_list->getInfo();
+        //echo "ControllerShopList";
+        $data = array();
+        $data['category'] = $this->load->controller('shop/category');
+        $data['header'] = $this->load->controller('common/header');
+        if($this->customer->isLogged())
+        {
+            $addressID = $this->customer->getAddressId();
+            $this->load->model('account/address');
+            $address = $this->model_account_address->getAddress($addressID);
+            $data['address'] = $address['city'].",".$address['address_1'];
+        }
+        else{
+            $data['address'] = "添加送餐地址";
+        }
+        //$data['product_overview'] = $this->load->controller('product/overview');
+        $this->response->setOutput($this->load->view('default/template/sfrest/list.tpl', $data));
+        //$this->response->setOutput($data['category']);
     }
 }
---
+++ Code/upload/catalog/model/sfrest/list.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Min
+ * Date: 2015/8/2
+ * Time: 16:04
+ */
+class ModelSfrestList extends Model{
+    public function getInfo()
+    {
+        return array("hello","world");
+    }
+}
