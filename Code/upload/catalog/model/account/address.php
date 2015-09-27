a6e9be3063d15513ccb99883090f5aa28d92b71c
xxx111x1 <122829105@qq.com>
2015-09-26 20:42:06 -0700

    Move address method from controller to model.

--- Code/upload/catalog/model/account/address.php
+++ Code/upload/catalog/model/account/address.php
@@ -162,4 +162,37 @@ class ModelAccountAddress extends Model {
 		$query = $this->db->query("SELECT distinct lat,lng,address FROM " . DB_PREFIX . "address_search_history WHERE customer_id = '" . (int)$this->customer->getId() . "' order by date_added desc limit 0,3 ");	
 		return $query->rows;
 	}
+	
+	function getDistance($lat1, $lng1, $lat2, $lng2)
+	{
+		$earthRadius = 6367000; //approximate radius of earth in meters
+	
+		/*
+		 Convert these degrees to radians
+		 to work with the formula
+		 */
+	
+		$lat1 = ($lat1 * pi() ) / 180;
+		$lng1 = ($lng1 * pi() ) / 180;
+	
+		$lat2 = ($lat2 * pi() ) / 180;
+		$lng2 = ($lng2 * pi() ) / 180;
+	
+		/*
+		 Using the
+		 Haversine formula
+	
+		 http://en.wikipedia.org/wiki/Haversine_formula
+	
+		 calculate the distance
+		 */
+	
+		$calcLongitude = $lng2 - $lng1;
+		$calcLatitude = $lat2 - $lat1;
+		$stepOne = pow(sin($calcLatitude / 2), 2) + cos($lat1) * cos($lat2) * pow(sin($calcLongitude / 2), 2);
+		$stepTwo = 2 * asin(min(1, sqrt($stepOne)));
+		$calculatedDistance = $earthRadius * $stepTwo;
+	
+		return round($calculatedDistance);
+	}
 }
