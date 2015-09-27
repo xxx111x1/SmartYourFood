c75e2cd1599d4d2311c57e928eaa3a7b2f4ec1a7
xxx111x1 <122829105@qq.com>
2015-09-26 20:40:26 -0700

    Add address history sql

--- Code/upload/install/opencart.sql
+++ Code/upload/install/opencart.sql
@@ -101,6 +101,15 @@ CREATE TABLE IF NOT EXISTS `oc_food_type` (
   PRIMARY KEY (`type_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
 
+CREATE TABLE `oc_address_search_history` (
+  `customer_id` int(11) NOT NULL,
+  `lat` decimal(10,8) DEFAULT NULL,
+  `lng` decimal(17,14) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `date_added` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 
 CREATE TABLE IF NOT EXISTS `oc_feedback` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
