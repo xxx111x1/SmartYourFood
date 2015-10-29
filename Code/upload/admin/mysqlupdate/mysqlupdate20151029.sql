f24199e53b421238b1b0a1fd3df0b700d3299429
xxx111x1 <minzou@live.com>
2015-10-29 21:09:08 +0800

    update mysql

--- Code/upload/admin/mysqlupdate/mysqlupdate20151029.sql
+++ Code/upload/admin/mysqlupdate/mysqlupdate20151029.sql
@@ -1,3 +1,2 @@
-user smartyourfood;
-alter table oc_order add  (store_telephone VARCHAR(100));
-alter table oc_order add  (store_address VARCHAR(512));
+alter table smartyourfood.oc_order add  (store_telephone VARCHAR(100));
+alter table smartyourfood.oc_order add  (store_address VARCHAR(512));
