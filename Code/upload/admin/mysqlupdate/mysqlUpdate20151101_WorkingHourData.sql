c4339e9a3bdcd862000934d373fdb1fdd365d7f2
xxx111x1 <122829105@qq.com>
2015-10-31 22:53:10 -0700

    Add working hour data.

---
+++ Code/upload/admin/mysqlupdate/mysqlUpdate20151101_WorkingHourData.sql
@@ -0,0 +1,14 @@
+update oc_restaurant_info set working_hour  = 'Open 24 Hours 7 Days'                                                   where restaurant_id = 7 ;
+update oc_restaurant_info set working_hour  = 'Mon: 5pm-1am, Tues:CLOSED, Wed-Sun:5pm-2am'                             where restaurant_id = 8 ;
+update oc_restaurant_info set working_hour  = 'Wed-Mon 11:00-15:00 and 17:00-22:00 Tue CLOSED'                         where restaurant_id = 9 ;
+update oc_restaurant_info set working_hour  = 'Monday - Sunday 11:30am-9:00pm'                                         where restaurant_id = 10;
+update oc_restaurant_info set working_hour  = 'Tue,Wed,Fri,Sat,Sun 12:00-22:00 Mon, Thur Closed'                       where restaurant_id = 11;
+update oc_restaurant_info set working_hour  = 'Tue-Sun 11:30-21:00 Mon CLOSED'                                         where restaurant_id = 12;
+update oc_restaurant_info set working_hour  = 'Monday - Sunday 11:00am-2:00am'                                         where restaurant_id = 13;
+update oc_restaurant_info set working_hour  = 'Mon-Thur,Sun 12:00-22:00 Fri-Sat 12:00-22:30'                           where restaurant_id = 14;
+update oc_restaurant_info set working_hour  = 'Mon, Wed-Sun 5pm-12am, Tues CLOSED'                                     where restaurant_id = 15;
+update oc_restaurant_info set working_hour  = 'Monday - Friday 11:30am-10:00pm Saturday - Sunday 10:30am-10:30pm'      where restaurant_id = 16;
+update oc_restaurant_info set working_hour  = 'Mon-Thur 11:30-21:30 Fri-Sun 11:30-22:00'                               where restaurant_id = 17;
+update oc_restaurant_info set working_hour  = 'Monday - Sunday 4:00pm-12:00am'                                         where restaurant_id = 18;
+update oc_restaurant_info set working_hour  = 'Monday - Sunday 24 hours'                                               where restaurant_id = 19;
+update oc_restaurant_info set working_hour  = 'Monday-Sunday 11:00am-11:00pm'                                          where restaurant_id = 20;
