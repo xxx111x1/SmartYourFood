0fc53d4a01c130b8bce271aeca00b17df73de67c
xxx111x1 <122829105@qq.com>
2015-11-01 01:27:37 -0700

    Change home page redirection.

--- Code/upload/catalog/view/javascript/sfheader.js
+++ Code/upload/catalog/view/javascript/sfheader.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 	$(document).on('click','#go-to-home,#logo', function(){
-		window.location.href='/index.php?route=common/sfhome&restaurant=1';
+		window.location.href='/index.php?route=common/sfhome';
 	});
 	$(document).on('click','#go-to-food', function(){
-		window.location.href='/index.php?route=common/list';
+		window.location.href='/index.php?route=common/list&restaurant=1';
 	});	
 	$(document).on('click','#go-to-order', function(){
 		window.location.href='/index.php?route=sfcheckout/checkout';
