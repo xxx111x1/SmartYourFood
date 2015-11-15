d627209d09c3ba5ab0b8373439688fe59716f705
xxx111x1 <minzou@live.com>
2015-11-15 13:07:21 +0800

    fix feeback bug.

--- Code/upload/catalog/model/catalog/feedback.php
+++ Code/upload/catalog/model/catalog/feedback.php
@@ -1,8 +1,12 @@
 <?php
 class ModelCatalogFeedback extends Model {
 	public function addFeedback($data) {
-		$this->event->trigger('pre.customer.add', $data);
+		$this->event->trigger('pre.feedback.add', $data);
+		$this->log->write('enter add feedback');
+		$queryStr = "INSERT INTO " . DB_PREFIX . "feedback SET  phone= '" . (int)$this->db->escape($data['phone']). "', email = '" . $this->db->escape($data['email']) . "', message = '" . $this->db->escape($data['message']) . "' , date_added = NOW()";
+		$this->log->write($queryStr);
 		$this->db->query("INSERT INTO " . DB_PREFIX . "feedback SET  phone= '" . (int)$this->db->escape($data['phone']). "', email = '" . $this->db->escape($data['email']) . "', message = '" . $this->db->escape($data['message']) . "' , date_added = NOW()");
-		$this->event->trigger('post.customer.add', $customer_id);
+		$this->log->write('after add feedback');
+		$this->event->trigger('post.feedback.add', $data);
 	}
 }
