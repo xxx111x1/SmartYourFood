01cfd4ffc6f84a1829caaaeebd397c3a0886c928
xxx111x1 <122829105@qq.com>
2015-11-29 03:27:09 -0800

    Add chinese language sql.

---
+++ Code/upload/admin/mysqlupdate/mysqlUpdate20151129_insert_language.sql
@@ -0,0 +1,17 @@
+INSERT INTO `smartyourfood`.`oc_language`
+(
+`name`,
+`code`,
+`locale`,
+`image`,
+`directory`,
+`sort_order`,
+`status`)
+VALUES
+('简体中文',
+'cn',
+'zh_CN',
+'cn.png',
+'chinese',
+'2',
+'1');
