ae3be3a83a639222c447850b04b9e44dd2efea87
xxx111x1 <122829105@qq.com>
2015-12-05 22:50:13 -0800

    Add update food english sql.

---
+++ Code/upload/admin/mysqlupdate/mysqlUpdate20151206_add_foo_tag.sql
@@ -0,0 +1,4 @@
+insert into smartyourfood.oc_food_tag values(40,'精美凉菜','Cold Appetizers');
+insert into smartyourfood.oc_food_tag values(41,'西域特色','Uygur Features');
+insert into smartyourfood.oc_food_tag values(42,'经典家常','Home Made Classics');
+insert into smartyourfood.oc_food_tag values(43,'串烧','Barbecue Skewers');
