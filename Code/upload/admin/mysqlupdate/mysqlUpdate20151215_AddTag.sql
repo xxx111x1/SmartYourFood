74555aa253005f4697a43a0db2f922a9cd4b6e6b
xxx111x1 <122829105@qq.com>
2015-12-15 02:33:07 -0800

    Add other foods.

---
+++ Code/upload/admin/mysqlupdate/mysqlUpdate20151215_AddTag.sql
@@ -0,0 +1,3 @@
+INSERT INTO `smartyourfood`.`oc_food_tag` (`tag_id`, `tag_name_cn`, `tag_name_en`) VALUES ('44', '肉夹馍', 'Stewed Marbled Meat & Flatbread');
+INSERT INTO `smartyourfood`.`oc_food_tag` (`tag_id`, `tag_name_cn`, `tag_name_en`) VALUES ('45', '拌面&凉皮', 'Cold Noodle (With No Soup)');
+INSERT INTO `smartyourfood`.`oc_food_tag` (`tag_id`, `tag_name_cn`, `tag_name_en`) VALUES ('46', '厨师推荐', 'Chef\'s Special');
